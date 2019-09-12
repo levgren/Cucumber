b461efe06dd22b1bc016b29c666ed9816a35fcf2
levgren <jekaobodianskyi@gmail.com>
2019-09-12 16:09:47 +0300

    +

--- src/main/java/pageObjects/GoogleTranslatePage.java
+++ src/main/java/pageObjects/GoogleTranslatePage.java
@@ -21,6 +21,10 @@ public class GoogleTranslatePage extends AbstractClass{
         driver.get(givenUrl);
         Allure.addAttachment("Navigating to Google search page", "given url: " + givenUrl + " is opened");
     }
+    public void goTol() {
+        driver.get(givenUrl);
+        Allure.addAttachment("Navigating to Google search page", "given url: " + givenUrl + " is opened");
+    }
 
     public void selectLanguage(String langFrom, String langTo){
         driver.findElement(selectTranslateLangFrom).click();
